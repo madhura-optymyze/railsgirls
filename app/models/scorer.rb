d3109291481410173fededca6411be027b4d3c4a
sneha <snehasweet87@gmail.com>
2016-01-30 15:43:59 +0530

    Added validations

--- app/models/scorer.rb
+++ app/models/scorer.rb
@@ -1,2 +1,4 @@
 class Scorer < ActiveRecord::Base
+  validates :mgr_rating, numericality: { :greater_than_or_equal_to => 0, :less_than_or_equal_to =>5 }, presence: true
+  validates :coworker_rating, numericality: { :greater_than_or_equal_to => 0, :less_than_or_equal_to =>5 }, presence: true
 end
